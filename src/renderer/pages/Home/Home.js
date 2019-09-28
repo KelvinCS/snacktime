694a5cdb7059713af0aff0c699d687e1e0ae44dc
Kelvin CS <kelvinh.job@gmail.com>
2019-09-27 22:30:14 -0300

    Criando página de home

---
+++ src/renderer/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container } from './Home.atoms';
+
+type Props = {};
+
+class Home extends Component<Props> {
+  render() {
+    return <Container></Container>;
+  }
+}
+
+export default Home;
