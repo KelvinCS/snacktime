af6d3a090d305bf7d282b710b22767f434445942
Kelvin CS <kelvinh.job@gmail.com>
2019-09-27 22:29:25 -0300

    Adicionando pagina do player

---
+++ src/renderer/pages/Player/Player.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from "react"
+import { Container } from "./Player.atoms"
+
+type Props = {}
+
+class Player extends Component<Props> {
+  render() {
+    return(
+      <div>
+        <h1>Opa, Cabron!</h1>
+      </div>
+    )
+  }
+}
+
+export default Player
+
