9b82ee463d9df225639e17372c40b7761b06f2c6
Kelvin CS <kelvinh.job@gmail.com>
2019-09-27 22:29:57 -0300

    Removendo comentarios desncessarios

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -8,14 +8,9 @@ import './theme/css/normalize.css';
 import './theme/css/nativize.css';
 import './theme/css/global.css';
 
-// import styled from 'styled-components';
-
-// import registerServiceWorker from './registerServiceWorker';
-
 ReactDOM.render(
   <Provider store={store}>
     <App history={history} />
   </Provider>,
   document.getElementById('app'),
 );
-// registerServiceWorker();
